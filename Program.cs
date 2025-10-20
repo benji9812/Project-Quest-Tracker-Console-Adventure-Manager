ce62ac7738063e0d8f96bc9c4e3bca9bb41a7b86
benji9812 <benjiw98@gmail.com>
2025-10-20 13:55:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Project_Quest_Tracker___Console_Adventure_Manager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
